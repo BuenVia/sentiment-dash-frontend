67bec2c55c94944f2dd291e3a381515346d526b9
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-09-10 12:04:26 +0200

    added in text to loading spinner to indicate backend spinning up

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="spinner-container">
+      <h3>This may take a moment whilst the backend wakes up...</h3>
       <div className="spinner"></div>
     </div>
   );
